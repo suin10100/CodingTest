1930f49d9c955ea9aa201cc886ee4dfb0c4e1568
suin10100 <94846590+suin10100@users.noreply.github.com>
2024-11-17 15:18:36 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,3 +1,4 @@
+/*
 function solution(rny_string) {
     var arr = rny_string.slice(0, rny_string.length)
     var ansArr = [];
@@ -5,4 +6,8 @@ function solution(rny_string) {
         ansArr[i] = arr[i] == 'm'? 'rn' : arr[i]
         }
     return ansArr.join("");
+}
+*/
+function solution(rny_string) {
+    return rny_string.replaceAll('m', 'rn');
 }
