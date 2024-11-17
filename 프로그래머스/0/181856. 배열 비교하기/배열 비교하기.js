dc0ac35795f2ae12b7ea3e54cb899324d77a4945
suin10100 <94846590+suin10100@users.noreply.github.com>
2024-11-17 14:28:37 +0900

    [level 0] Title: 배열 비교하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,34 +1,18 @@
-/*
 function solution(arr1, arr2) {
   var answer = 0;
 
   if (arr1.length == arr2.length) {
     const sum_arr1 = arr1.reduce((a, b) => a + b, 0);
     const sum_arr2 = arr2.reduce((a, b) => a + b, 0);
-    if (sum_arr1 > sum_arr2) {
-      answer = 1;
-    } else if (sum_arr1 < sum_arr2) {
-      answer = 1;
-    } else answer = 0;
-  } else answer = arr1.length > arr2.length ? 1 : -1;
-
-  return answer;
-}
-
-*/
-
-function solution(arr1, arr2) {
-
-    if(arr1.length !== arr2.length) {
-        return arr1.length > arr2.length ? 1 : -1; 
-    } 
-
-    const sum_arr1 = arr1.reduce((a, b) => a + b, 0);
-    const sum_arr2 = arr2.reduce((a, b) => a + b, 0);
     if (sum_arr1 == sum_arr2) {
-        return 0
+      answer = 0;
     }
     else {
-        return sum_arr1 > sum_arr2 ? 1 : -1
+        answer = sum_arr1 > sum_arr2 ? 1 : -1;
     }
+    
+  } 
+  else { answer = arr1.length > arr2.length ? 1 : -1; }
+
+  return answer;
 }
