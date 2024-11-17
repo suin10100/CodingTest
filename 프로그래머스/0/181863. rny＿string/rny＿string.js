3efe0dd59d4956e568f210e55405885549879ba7
suin10100 <94846590+suin10100@users.noreply.github.com>
2024-11-17 15:08:38 +0900

    [level 0] Title: rny_string, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -0,0 +1,8 @@
+function solution(rny_string) {
+    var arr = rny_string.slice(0, rny_string.length)
+    var ansArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        ansArr[i] = arr[i] == 'm'? 'rn' : arr[i]
+        }
+    return ansArr.join("");
+}
