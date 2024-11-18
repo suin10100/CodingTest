805b6f61a0521b5b4804c437530fe0d87976b65a
suin10100 <94846590+suin10100@users.noreply.github.com>
2024-11-18 16:46:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -5,12 +5,10 @@ function solution(A, B) {
     }
     else( A !== B) 
         for(let i = 0; i < A.length; i++) {
-            var save = a[a.length - 1]; // e / l
-            console.log(a);  
+            var save = a[a.length - 1]; // e / l 
             a.pop(); // appl / eapp
             var string = save + a.join(''); // eappl / leapp
             a = [...string];
-            console.log(string);
             if(string == B) {
                 return i + 1;
             }
