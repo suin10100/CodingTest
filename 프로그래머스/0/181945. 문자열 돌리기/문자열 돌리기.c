8c8f4bc36ada333dccde96b53f18423c38b8c9c1
Suin <94846590+suin10100@users.noreply.github.com>
2025-05-20 17:26:57 +0900

    [level 0] Title: 문자열 돌리기, Time: 3.55 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
@@ -2,10 +2,8 @@
 #define LEN_INPUT 11
 
 int main(void) {
-    char s1[10];//100까지 입력 가능
-    scanf("%10s", s1);// str 입력
-    for(int i=0; s1[i] != '\0'; i++) {
-        printf("%c\n", s1[i]);
-    }
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+
     return 0;
 }
