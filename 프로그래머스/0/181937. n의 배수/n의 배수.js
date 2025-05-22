b15b38750094d2787681f2c90328888f00f2a2d3
Suin <94846590+suin10100@users.noreply.github.com>
2025-05-22 18:16:36 +0900

    [level 0] Title: n의 배수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.js
+++ 프로그래머스/0/181937. n의 배수/n의 배수.js
@@ -1,11 +1,3 @@
 function solution(num, n) {
-    
-    var answer = 0;
-    
-    if(num % n == 0 ) {
-        answer = 1
-    }
-    else 
-        answer = 0;
-    return answer;
+    return num % n == 0 ? 1 : 0;
 }
