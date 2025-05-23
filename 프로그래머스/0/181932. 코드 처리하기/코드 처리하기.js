590d6a467f0e996ce6df6721bb0ec49f95c7f04d
Suin <94846590+suin10100@users.noreply.github.com>
2025-05-24 00:56:42 +0900

    [level 0] Title: 코드 처리하기, Time: 9.07 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,23 +1,16 @@
 function solution(code) {
-    let mode = 0;
     let ret = "";
-    let count = 0;
-    for (let i = 0; i<code.length; i++) {
-        if( code[i] === "1") {
-            mode = mode === 0 ? 1 : 0;
-            continue;
-        }
-       if (mode === 0) {
-            if (count % 2 === 0) {
-                ret += code[i];
-            }
-            count++;
-        } else {
-            if (count % 2 === 0) {
-                ret += code[i];
-            }
-            count++;
+    let mode = 0;
+
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === "1") {
+            mode = 1 - mode;
+        } else if (mode === 0 && i % 2 === 0) {
+            ret += code[i];
+        } else if (mode === 1 && i % 2 !== 0) {
+            ret += code[i];
         }
     }
-    return ret === "" ? "EMPTY": ret;
+
+    return ret === "" ? "EMPTY" : ret;
 }
