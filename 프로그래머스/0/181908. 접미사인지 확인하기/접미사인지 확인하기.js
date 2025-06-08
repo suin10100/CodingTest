3ebdbff65fc4386a5602617d03ab44d2ed651538
Suin <94846590+suin10100@users.noreply.github.com>
2025-06-09 04:57:29 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,3 @@
 function solution(my_string, is_suffix) {
-    return [...my_string].map((_, i) => my_string.slice(i)).includes(is_suffix) ? 1 : 0;
+    return my_string.endsWith(is_suffix) ? 1 : 0;
 }
